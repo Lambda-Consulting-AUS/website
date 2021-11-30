5b919709a94be72bd19dfb522dd2bd3e48987b1f
Oliver Balfour <oliver.leo.balfour@gmail.com>
2021-11-30 21:57:03 +1100

    mobile friendly footer

--- src/footer.js
+++ src/footer.js
@@ -7,13 +7,13 @@ export default function Footer () {
     <footer>
       <div className='container'>
         <Row justify='space-between' align='middle'>
-          <Col span={11}>
+          <Col lg={11} md={11} sm={11} xs={24}>
             <img src='logo-square.svg' width='200' height='200'
               alt='Lambda Consulting logo' />
             <br/>
             Copyright © Lambda Consulting 2022
           </Col>
-          <Col span={11}>
+          <Col lg={11} md={11} sm={11} xs={24}>
             <h2 style={{ fontSize: '1.4rem' }}>Contact</h2>
             <p style={{ fontSize: '1rem' }}>Do you have any questions? Reach out to us at</p>
             <MailtoLink /><br />
